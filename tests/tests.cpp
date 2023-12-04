91ef81c8b23bba470b80a6a2bef644f80a5f7e49
markraskin <mrask2@illinois.edu>
2023-12-04 04:25:09 +0000

    created the 100x100 test case and used bfs for first time

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,13 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
 #include "Grid.h"
-
+#include "BFSgrid.h"
 TEST_CASE("3x3 Correct Shortest Path from top left to bottom right", "[weight=10][valgrind][3x3]") {
     // 3x3 maze is the grid of nodes created from the 3x3.txt dataset, where each number 1-9 represents a passable node with weight and each 0 represents the start and end
     // findPath will return a vector of nodes that represent the shortest path from pointA to pointB
     // 3x3shortestPath will be a vector of points that correspond to the shortest path from the top left most points (0,0) to the bottom left most point (5,0)
     Grid threeGrid;
-    threeGrid.readFromFile("../data/3x3.txt", 3, 3);
-    threeGrid.createPointMaze();
+    threeGrid.readFromFile("../data/3x3.txt", 0);
+    threeGrid.createPointMaze(2,2);
     Point start = threeGrid(0,0);
     Point end = threeGrid(2,2);
 
@@ -18,6 +18,8 @@ TEST_CASE("3x3 Correct Shortest Path from top left to bottom right", "[weight=10
     }
 }
 
+
+
 TEST_CASE("3x3 Correct Heuristic Calculations") {
 
     // creating a grid from the 3x3maze.txt image
@@ -26,10 +28,39 @@ TEST_CASE("3x3 Correct Heuristic Calculations") {
     Point endPoint(2, 2, 1, 0); // expected h value of endPoint
 
     Grid threeGrid;
-    threeGrid.readFromFile("../data/3x3.txt", 3, 3);
-    threeGrid.createPointMaze();
+    threeGrid.readFromFile("../data/3x3.txt", 0);
+    threeGrid.createPointMaze(2,2);
     Point start = threeGrid(0,0);
     Point end = threeGrid(2,2);
+    
+    // creating a vector of points with known heuristics
+    vector<Point> knownHeuristics = {startPoint, midPoint, endPoint}; // example known heuristic value for startPoint
+
+    // Calculate and compare heuristic values for each point
+    for (const auto& point: knownHeuristics) {
+
+        double expectedHeuristic = point.getH(); //h_ is the heursitic value
+        
+        double calculatedHeuristic = threeGrid(point.getXY().first, point.getXY().second).getH();
+        
+        // Check if the calculated heuristic matches the expected value
+        REQUIRE(calculatedHeuristic == expectedHeuristic);
+    }
+}
+
+TEST_CASE("3x3 Correct Backwards Heuristic Calculations") {
+
+    // creating a grid from the 3x3maze.txt image
+    
+    Point startPoint(2, 2, 1, 0); //expected h value of startPoint
+    Point midPoint(1, 1, 1, 2); //expected h value of midPoint
+    Point endPoint(0, 0, 1, 4); // expected h value of endPoint
+
+    Grid threeGrid;
+    threeGrid.readFromFile("../data/3x3.txt",0);
+    threeGrid.createPointMaze(2,2);
+    Point start = threeGrid(2,2);
+    Point end = threeGrid(0,0);
     
     // creating a vector of points with known heuristics
     vector<Point> knownHeuristics = {startPoint, midPoint, endPoint}; // example known heuristic value for startPoint
@@ -52,18 +83,48 @@ TEST_CASE("5x5 Correct Shortest Path from top left to bottom right", "[weight=10
     // findPath will return a vector of nodes that represent the shortest path from pointA to pointB
     // 3x3shortestPath will be a vector of points that correspond to the shortest path from the top left most points (0,0) to the bottom left most point (5,0)
     Grid fiveGrid;
-    fiveGrid.readFromFile("../data/5x5.txt", 5, 5);
-    fiveGrid.createPointMaze();
+    fiveGrid.readFromFile("../data/5x5.txt", 0);
+    fiveGrid.createPointMaze(4,4);
     Point start = fiveGrid(0,0);
     Point end = fiveGrid(4,4);
 
-    vector<pair<int,int>> solution = {{0,0}, {0,1}, {1,1}, {1,2}, {2,2}, {3,2}, {3,3}, {3,4}, {4,4}};
-    vector<pair<int,int>> threeShortestPath = fiveGrid.solveMaze(start, end);
+    vector<pair<int,int>> solution = {{0,0}, {0,1}, {1,1}, {1,2}, {2,2}, {2,3}, {3,3}, {3,4}, {4,4}};
+    vector<pair<int,int>> fiveShortestPath = fiveGrid.solveMaze(start, end);
     for (size_t i = 0; i < solution.size(); i++) {
-        REQUIRE(solution[i].first == threeShortestPath[i].first);
-        REQUIRE(solution[i].second == threeShortestPath[i].second);
+        REQUIRE(solution[i].first == fiveShortestPath[i].first);
+        REQUIRE(solution[i].second == fiveShortestPath[i].second);
     }
 }
+
+
+
+
+TEST_CASE("100x100 Correct Shortest Path from top left to bottom right", "[weight=10][valgrind][3x3]") {
+    // 3x3 maze is the grid of nodes created from the 3x3.txt dataset, where each number 1-9 represents a passable node with weight and each 0 represents the start and end
+    // findPath will return a vector of nodes that represent the shortest path from pointA to pointB
+    // 3x3shortestPath will be a vector of points that correspond to the shortest path from the top left most points (0,0) to the bottom left most point (5,0)
+    Grid hundredGrid;
+    hundredGrid.readFromFile("../data/100x100.txt", 0);
+    hundredGrid.createPointMaze(99,99);
+    Point start = hundredGrid(0,0);
+    Point end = hundredGrid(99,99);
+
+
+    BFSgrid bfsHundredGrid;
+    bfsHundredGrid.readFromFile("../data/100x100.txt" , 0);
+    bfsHundredGrid.createPointMaze();
+    
+    
+
+    vector<pair<int,int>> solution = bfsHundredGrid.solveMaze(start, end);
+    vector<pair<int,int>> hundredShortestPath = hundredGrid.solveMaze(start, end);
+    for (size_t i = 0; i < solution.size(); i++) {
+        REQUIRE(solution[i].first == hundredShortestPath[i].first);
+        REQUIRE(solution[i].second == hundredShortestPath[i].second);
+    }
+}
+
+
 /*
 TEST_CASE("Straight Path with No Obstacles") {
     // testing on a straight path with no obstacles
